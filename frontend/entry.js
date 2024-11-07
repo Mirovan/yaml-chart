933cfa75e6afaab932d8c90bc1fb6eb7b2354ecd
Maksim Milovanov <maksim.milovanov@domrf.ru>
2024-11-07 17:50:06 +0300

    вычисление координаты вставки объектов

--- frontend/entry.js
+++ frontend/entry.js
@@ -3,10 +3,55 @@ import "./css/styles.scss";
 import Konva from 'konva';
 import YAML from 'yaml';
 
-document.addEventListener('DOMContentLoaded', function () {
+const defaultBoxWidth = 100;
+const defaultBoxHeight = 50;
 
-    const yamlData = YAML.parse(document.getElementById("yaml-text").value);
-    console.log(yamlData);
+/*
+* Расчет координат для каждого объекта.
+* Координаты расстановки объектов - относительные
+* */
+function calcLayout(objects, startX, startY) {
+    console.log(objects);
+    let currentX = 0;
+    let currentY = 0;
+
+    //Для каждого объекта вычисляем куда его поставить
+    for (let key of objects.keys()) {
+        const obj = objects.get(key);
+
+        //Если у объекта дочерние объекты, то вычисляем куда их поставить
+        if (obj.objects != null) {
+            obj.objects = calcLayout(obj.objects, 0, 0);
+        }
+        if (obj.x == null) {
+            obj.x = 0;
+        }
+        if (obj.y == null) {
+            obj.y = 0;
+        }
+        obj.y += currentY + defaultBoxHeight;
+
+        //Обновляем Y для объекта
+        currentY += defaultBoxHeight;
+    }
+}
+
+
+/*
+* Преобразовывает рекурсивно объект в Map с вложенными объектам
+* */
+function toMap(objects) {
+    let map = new Map(Object.entries(objects));
+    for (let key of map.keys()) {
+        if (map.get(key).objects != null) {
+            map.get(key).objects = toMap(map.get(key).objects);
+        }
+    }
+    return map;
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
 
     const stage = new Konva.Stage({
         container: 'watman',
@@ -20,15 +65,50 @@ document.addEventListener('DOMContentLoaded', function () {
     });
     stage.add(canvasLayer);
 
-    const rect = new Konva.Rect({
-        x: 20,
-        y: 20,
-        width: 100,
-        height: 50,
-        fill: '#eeeeee',
-        stroke: 'black',
-        strokeWidth: 2,
-    });
 
-    canvasLayer.add(rect);
+    const yamlData = YAML.parse(document.getElementById("yaml-text").value);
+    let data = new Map(Object.entries(yamlData.data));
+    let relations = new Map(Object.entries(yamlData.relations));
+
+    const objects = toMap(data.get("objects"));
+
+    if (data.get("layout") === "column") {
+        calcLayout(objects, 0, 0);
+    }
+
+    /*
+    //перебираем все связи
+    for (const key of relations.keys()) {
+        const relation = relations.get(key);
+        const from = objects.get(relation.from);
+        const to = objects.get(relation.to);
+
+        from.x = 0;
+        from.y = 0;
+
+        //По связи рисуем два объекта исходя их связи
+        const rect1 = new Konva.Rect({
+            x: from.x,
+            y: from.y,
+            width: 100,
+            height: 50,
+            fill: '#eeeeee',
+            stroke: 'black',
+            strokeWidth: 2,
+        });
+        canvasLayer.add(rect1);
+
+        const rect2 = new Konva.Rect({
+            x: to.x,
+            y: to.y,
+            width: 100,
+            height: 50,
+            fill: '#eeeeee',
+            stroke: 'black',
+            strokeWidth: 2,
+        });
+        canvasLayer.add(rect2);
+    }
+     */
+
 });
---
+++ frontend/object.js
@@ -0,0 +1,6 @@
+class Box {
+    constructor(id, properties) {
+        this.id = id;
+        this.properties = properties;
+    }
+}
