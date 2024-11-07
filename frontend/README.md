a8146dcdfdf72cb30a4ad49411a46e48a3fa27ca
Maksim Milovanov <maksim.milovanov@domrf.ru>
2024-11-07 12:48:33 +0300

    Add yaml, Konva libs

--- frontend/README.md
+++ frontend/README.md
@@ -7,5 +7,9 @@ npm i html-webpack-plugin
 npm i --save bootstrap @popperjs/core
 npm i --save-dev autoprefixer css-loader postcss-loader sass sass-loader style-loader
 npm i bootstrap
+npm i yaml
+npm i konva
 npm run build
 ```
+
+`npx webpack serve` - запуск Web-сервера
--- frontend/entry.js
+++ frontend/entry.js
@@ -1,2 +1,34 @@
 import "./css/styles.scss";
 
+import Konva from 'konva';
+import YAML from 'yaml';
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    const yamlData = YAML.parse(document.getElementById("yaml-text").value);
+    console.log(yamlData);
+
+    const stage = new Konva.Stage({
+        container: 'watman',
+        width: 800,
+        height: 600,
+    });
+
+    const canvasLayer = new Konva.Layer({
+        x: 0,
+        y: 0,
+    });
+    stage.add(canvasLayer);
+
+    const rect = new Konva.Rect({
+        x: 20,
+        y: 20,
+        width: 100,
+        height: 50,
+        fill: '#eeeeee',
+        stroke: 'black',
+        strokeWidth: 2,
+    });
+
+    canvasLayer.add(rect);
+});
