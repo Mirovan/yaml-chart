f2f0af0ca437dc2d9d981b67f8a38b372273a39e
Maksim Milovanov <maksim.milovanov@domrf.ru>
2024-11-15 22:43:53 +0300

    Рефакторинг расстановки объектов

--- frontend/entry.js
+++ frontend/entry.js
@@ -12,6 +12,11 @@ const defaultMarginY = 10;
 /*Инициализация объекта и задание его границ*/
 
 function initObject(obj, lastObj, parent) {
+    if (obj.name === "ingress2") {
+        console.log(obj);
+        console.log(lastObj);
+        console.log(parent);
+    }
     //Начальная координата для вставки объекта
     let startObjX = defaultMarginX;
     let startObjY = defaultMarginY;
@@ -55,15 +60,45 @@ function initObject(obj, lastObj, parent) {
 * Координаты расстановки объектов - относительные
 * startX, startY - начальные координаты расчета установки относительно родителя
 * */
-function calcLayout(objects, parent) {
-    console.log(parent);
+function calcLayout(object, lastObj, parent) {
+    object = initObject(object, lastObj, parent);
 
-    let lastObj = null;
+    //Для каждого внутреннего объекта рассчитываем куда его поставить
+    if (Func.notNull(object.objects)) {
+        let innerObjects = new Map(Object.entries(object.objects));
+        lastObj = null;
+        for (let key of innerObjects.keys()) {
+            //Считаем координаты объекта
+            const innerObj = calcLayout(innerObjects.get(key), lastObj, object);
+            //Обновляем его в коллекции родителя
+            innerObjects.set(key, innerObj);
+            lastObj = innerObj;
+        }
 
+        object.objects = innerObjects;
+
+        let widthObjX = object.width;
+        let heightObjX = object.height;
+
+        //Когда посчитали координаты всех внутренних объектов - вычисляем размер родителя-контейнера
+        for (let k of innerObjects.keys()) {
+            const tempObj = innerObjects.get(k);
+            widthObjX = Math.max(widthObjX, tempObj.x + tempObj.width);
+            heightObjX = Math.max(heightObjX, tempObj.y + tempObj.height);
+        }
+
+        //обновляем вторую границу родительского объекта
+        object.width = widthObjX + defaultMarginX;
+        object.height = heightObjX + defaultMarginY;
+    }
+
+
+
+    /*
     //Для каждого объекта вычисляем куда его поставить
-    for (let key of objects.keys()) {
+    for (let key of innerObjects.keys()) {
         //Инициализируем координаты и размер объекта
-        const obj = initObject(objects.get(key), lastObj, parent);
+        const obj = initObject(innerObjects.get(key), lastObj, parent);
 
         //Если у объекта дочерние объекты, то вычисляем куда их поставить
         if (Func.notNull(obj.objects)) {
@@ -86,37 +121,44 @@ function calcLayout(objects, parent) {
 
         lastObj = obj;
     }
-    return objects;
+     */
+
+    return object;
 }
 
 
 /*
 * Отрисовка
+* object - рисуемый объект
+* parent - родительский объект
+* dx, dy - отступ с учетом относительных координат
+* canvasLayer - canvas для рисования
 * */
-function draw(objects, parent, dx, dy, canvasLayer) {
+function draw(object, parent, dx, dy, canvasLayer) {
     if (Func.notNull(parent)) {
         dx += parent.x;
         dy += parent.y;
     }
 
+    const rect = new Konva.Rect({
+        x: object.x + dx,
+        y: object.y + dy,
+        width: object.width,
+        height: object.height,
+        fill: object.color,
+        stroke: 'black',
+        strokeWidth: 2,
+        draggable: true
+    });
+    canvasLayer.add(rect);
+
     //Перебираем все объекты
-    for (let key of objects.keys()) {
-        const obj = objects.get(key);
+    const innerObjects = object.objects;
+    if (innerObjects != null) {
+        for (let key of innerObjects.keys()) {
+            const innerObj = innerObjects.get(key);
 
-        const rect = new Konva.Rect({
-            x: obj.x + dx,
-            y: obj.y + dy,
-            width: obj.width,
-            height: obj.height,
-            fill: obj.color,
-            stroke: 'black',
-            strokeWidth: 2,
-            draggable: true
-        });
-        canvasLayer.add(rect);
-
-        if (Func.notNull(obj.objects)) {
-            draw(obj.objects, obj, dx, dy, canvasLayer);
+            draw(innerObj, object, dx, dy, canvasLayer);
         }
     }
 }
@@ -126,7 +168,6 @@ function draw(objects, parent, dx, dy, canvasLayer) {
 
 
 document.addEventListener('DOMContentLoaded', function () {
-
     const stage = new Konva.Stage({
         container: 'watman',
         width: 800,
@@ -144,14 +185,23 @@ document.addEventListener('DOMContentLoaded', function () {
     let data = new Map(Object.entries(yamlData.data));
     let relations = new Map(Object.entries(yamlData.relations));
 
-    const parent = Func.toMap(data);
-    const objects = Func.toMap(data.get("objects"));
+    // const parent = Func.toMap(data);
+    // const objects = Func.toMap(data.get("objects"));
+
+    // console.log(yamlData.data);
+    // console.log(data);
+    // console.log(data.get("objects"));
+    // console.log(objects);
+    // console.log("=============");
+
+    // console.log(data);
+    // console.log(objects);
 
     //Вычисление координат
-    calcLayout(objects, parent);
+    const object = calcLayout(yamlData.data, null, null);
 
     //Отрисовка
-    draw(objects, null, 0, 0, canvasLayer);
+    draw(object, null, 0, 0, canvasLayer);
 
 
     /*
